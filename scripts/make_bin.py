59bba43f769c57cf88e444a922031acca2e5759b
stulle123 <dawin.schmidt@mailbox.org>
2023-04-12 16:38:28 +0200

    Add simple improvements

--- scripts/make_bin.py
+++ scripts/make_bin.py
@@ -3,12 +3,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("csv_file", help="path to csv file")
-parser.add_argument("output", help="filename for the result")
+parser.add_argument("output", help="output file name")
 args = parser.parse_args()
 
-
-with open(args.csv_file) as f:
-    with open(args.output , "wb") as w:
-        for line in f:
+try:
+    with open(args.csv_file, "r") as f, open(args.output, "wb") as w:
+        for line in f.read():
             line = re.sub(r'\"SPI\",|\s|\n|0x', '', line)
             w.write(bytes.fromhex(line))
+except OSError as error:
+    print(f"Could not read CSV and/or write to output file: {error}")
